e986f7a20e8d19846f6f94562f499a52b89df5a2
Michael Tuexen <tuexen@fh-muenster.de>
2016-08-22 03:29:06 +0200

    Improve locking.

--- netinet/sctp_output.c
+++ netinet/sctp_output.c
@@ -13609,7 +13609,8 @@ sctp_lower_sosend(struct socket *so,
 		}
 		SCTP_INP_RUNLOCK(inp);
 	} else if (sinfo_assoc_id) {
-		stcb = sctp_findassociation_ep_asocid(inp, sinfo_assoc_id, 0);
+		stcb = sctp_findassociation_ep_asocid(inp, sinfo_assoc_id, 1);
+		hold_tcblock = 1;
 	} else if (addr) {
 		/*-
 		 * Since we did not use findep we must
@@ -14458,6 +14459,10 @@ skip_preblock:
 			}
 		}
 		SCTP_TCB_SEND_LOCK(stcb);
+		if (stcb->asoc.state & SCTP_STATE_ABOUT_TO_BE_FREED) {
+			SCTP_TCB_SEND_UNLOCK(stcb);
+			goto out_unlocked;
+		}
 		if (sp) {
 			if (sp->msg_is_complete == 0) {
 				strm->last_msg_incomplete = 1;
