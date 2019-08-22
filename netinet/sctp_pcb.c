d53fcfccbca953f747c547f4fd1c3e4ce795c128
Michael Tuexen <tuexen@fh-muenster.de>
2019-08-22 09:04:41 +0200

    Sync with usrsctp.

--- netinet/sctp_pcb.c
+++ netinet/sctp_pcb.c
@@ -4734,7 +4734,7 @@ sctp_add_remote_addr(struct sctp_tcb *stcb, struct sockaddr *newaddr,
 #endif
 #if defined(__Userspace__)
 			case AF_CONN:
-				net->mtu = 1280;
+				net->mtu = 1200;
 				break;
 #endif
 			default:
@@ -6635,7 +6635,7 @@ sctp_netisr_hdlr(struct mbuf *m, uintptr_t source)
 #endif
 
 void
-sctp_pcb_init()
+sctp_pcb_init(int start_threads)
 {
 	/*
 	 * SCTP initialization for the PCB structures should be called by
@@ -6851,7 +6851,8 @@ sctp_pcb_init()
 	mbuf_initialize(NULL);
 	atomic_init();
 #if defined(INET) || defined(INET6)
-	recv_thread_init();
+	if (start_threads)
+		recv_thread_init();
 #endif
 #endif
 }
